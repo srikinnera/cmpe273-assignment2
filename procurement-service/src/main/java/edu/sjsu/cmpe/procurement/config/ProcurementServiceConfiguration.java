166ae4ce8f5a843a7bf9f00d281e053cbb7b323d
srikinnera <skinnera92@gmail.com>
2014-04-04 20:53:21 -0700

    procurement

--- procurement-service/src/main/java/edu/sjsu/cmpe/procurement/config/ProcurementServiceConfiguration.java
+++ procurement-service/src/main/java/edu/sjsu/cmpe/procurement/config/ProcurementServiceConfiguration.java
@@ -17,7 +17,22 @@ public class ProcurementServiceConfiguration extends Configuration {
     @NotEmpty
     @JsonProperty
     private String stompTopicPrefix;
+    @NotEmpty
+    @JsonProperty
+    private String apolloHost;
+    @NotEmpty
+    @JsonProperty
+    private String apolloPort;
+    @NotEmpty
+    @JsonProperty
+    private String apolloUser;
+    
 
+    @NotEmpty
+    @JsonProperty
+    private String apolloPassword;
+
+   
     @Valid
     @NotNull
     @JsonProperty
@@ -28,14 +43,14 @@ public class ProcurementServiceConfiguration extends Configuration {
      * @return
      */
     public JerseyClientConfiguration getJerseyClientConfiguration() {
-	return httpClient;
+    return httpClient;
     }
 
     /**
      * @return the stompQueueName
      */
     public String getStompQueueName() {
-	return stompQueueName;
+    return stompQueueName;
     }
 
     /**
@@ -43,15 +58,46 @@ public class ProcurementServiceConfiguration extends Configuration {
      *            the stompQueueName to set
      */
     public void setStompQueueName(String stompQueueName) {
-	this.stompQueueName = stompQueueName;
+    this.stompQueueName = stompQueueName;
     }
 
     public String getStompTopicPrefix() {
-	return stompTopicPrefix;
+    return stompTopicPrefix;
     }
 
     public void setStompTopicPrefix(String stompTopicPrefix) {
-	this.stompTopicPrefix = stompTopicPrefix;
+    this.stompTopicPrefix = stompTopicPrefix;
+    }
+    public String getApolloHost() {
+        return apolloHost;
+    }
+
+    public void setApolloHost(String apolloHost) {
+        this.apolloHost = apolloHost;
+    }
+
+    public String getApolloPort() {
+        return apolloPort;
+    }
+
+    public void setApolloPort(String apolloPort) {
+        this.apolloPort = apolloPort;
+    }
+
+    public String getApolloUser() {
+        return apolloUser;
+    }
+
+    public void setApolloUser(String apolloUser) {
+        this.apolloUser = apolloUser;
+    }
+
+    public String getApolloPassword() {
+        return apolloPassword;
+    }
+
+    public void setApolloPassword(String apolloPassword) {
+        this.apolloPassword = apolloPassword;
     }
 
 }
