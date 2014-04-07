52a079725554f7b2c1e4a111dfe2b6f9baa55173
srikinnera <skinnera92@gmail.com>
2014-04-06 22:04:15 -0700

    assignment2

--- procurement-service/src/main/java/edu/sjsu/cmpe/procurement/jobs/ProcurementSchedulerJob.java
+++ procurement-service/src/main/java/edu/sjsu/cmpe/procurement/jobs/ProcurementSchedulerJob.java
@@ -44,7 +44,7 @@ public class ProcurementSchedulerJob extends Job {
 			// .resource("http://54.215.133.131:9000/orders");
 			// String body = "{\"id\":\"32304\",\"order_book_isbns\":[1]}";
 
-			long waitUntil = 500;
+			long waitUntil = 5000;
 			while (true) {
 				log.info("before consuming");
 				msg = ProcurementService.consumer.receive(waitUntil);
@@ -56,33 +56,49 @@ public class ProcurementSchedulerJob extends Job {
 					body += smsg.getFrame().contentAsString().substring(10)
 							+ ",";
 					hasMsg = true;
+					
 				} else if (msg == null) {
 					System.out.println("No Books in the Queue");
 					System.out
 							.println("No new messages. Exiting due to timeout - "
 									+ waitUntil / 1000 + " sec");
-					hasMsg = false;
+					//hasMsg = false;
 					break;
 				} else {
 					System.out.println("Unexpected message type: "
 							+ msg.getClass());
 				}
 			}
-
+			System.out.println(hasMsg);
+			System.out.println("body 1 " +body);
 			if (hasMsg) {
 				//// System.out.println(input);
 				body += "]}";
-
+				System.out.println("body 2 "+body);
 				int position = body.lastIndexOf(",");
 				body = replaceCharAt(body, position, "");
-				ClientResponse strResponse = null;
-				try {
-					strResponse = ProcurementService.jerseyClient.create()
+				System.out.println("body 3 "+body);
+			/*	ClientResponse strResponse = Client.create()
 							.resource("http://54.215.210.214:9000/orders")
 							.type("application/json")
+							.post(ClientResponse.class, body);*/
+				try {
+					System.out.println("inside post method");
+					Client client = Client.create();
+					WebResource webResource = client.resource("http://54.215.133.131:9000/orders");
+					ClientResponse response = webResource.type("application/json")
 							.post(ClientResponse.class, body);
+					if (response.getStatus() != 200) {
+						throw new RuntimeException("Failed : HTTP error code : "
+								+ response.getStatus());
+					}
+					System.out.println("Output from Server .... \n");
+					String output = response.getEntity(String.class);
+					System.out.println(output);
 				} catch (Exception e) {
+					e.printStackTrace();
 				}
+				
 
 				/*
 				 * ClientResponse
@@ -94,13 +110,13 @@ public class ProcurementSchedulerJob extends Job {
 				// String strResponse =
 				// ProcurementService.jerseyClient.resource(
 				// "http://ip.jsontest.com/").get(String.class);
-				if (strResponse.getStatus() == 200) {
+			/*	if (strResponse.getStatus() == 200) {
 					log.debug("Response from Publisher:{}",
 							strResponse.getEntity(String.class));
 				} else {
 					log.debug("HTTP Error Code returned as:{}",
 							strResponse.getStatus());
-				}
+				}*/
 
 			} // end hasMsg if
 
@@ -119,11 +135,14 @@ public class ProcurementSchedulerJob extends Job {
 	}
 
 	private void doGetFromPublisher() throws JMSException {
+		
+		System.out.println("I am here");
 		Map<String, Book[]> collector;
 		ClientResponse strResponse = ProcurementService.jerseyClient.create()
 				.resource("http://54.215.133.131:9000/orders/32304")
 				.accept("application/json").type("application/json")
 				.get(ClientResponse.class);
+		System.out.println("Response is "+strResponse);
 		// log.debug("Response from Get Publisher:{}",
 		// strResponse.getEntity(String.class));
 		// TODO Auto-generated method stub
